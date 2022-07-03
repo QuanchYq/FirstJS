f32a56c8732118813b6091429169191b5080c901
Aikyn <210101044@stu.sdu.edu.kz>
2022-07-03 17:50:43 +0500

    updated v2

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-const arr = [46,2,3,4];
-arr.sort(compare)
-console.log(arr)
+const personalPlanPeter = {
+    name: 'Aikyn',
+    age: '18',
+    skills: {
+        languages: ['ru','eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function (plan) {
+        const {age} = plan;
+        const {languages} = plan.skills
+        let str =  `I am ${age} and i use languages`;
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()}`;
+        });
+        return str;
+    }
+}
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
-function compare(a,b) {
-  return a - b;
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
 }
 
-// arr.forEach(function(item,i,arr) {
-//   console.log(`${i} : ${item} inside massive ${arr}`)
-// })
-
-
-// arr[99] = 0;
-//
-// console.log(arr.length);
-
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-//
-// for (let value of arr) {
-//   console.log(value)
-// }
-
-// const str = prompt("",'');
-// const products = str.split(", ");
-//
-// products.sort()
-//
-// console.log(products.join(';'))
+showExperience(personalPlanPeter)
