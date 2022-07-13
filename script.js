d49a2b8e8862b3b1a9d08110043dd34fb9a4b726
Aikyn <210101044@stu.sdu.edu.kz>
2022-07-13 19:23:49 +0500

    updated v2

--- script.js
+++ script.js
@@ -1,61 +1,19 @@
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-}
-// function a() {
-//     let area = [];
-//     shoppingMallData.shops.forEach((arr,i) => {
-//         const a = arr.width * arr.length;
-//         area[i] = a
-//     });
-//
-//     let mainArea = 0;
-//     for (i of area) {
-//         mainArea += i
-//     }
-//     console.log(mainArea)
-// }
-// a()
+const box = document.getElementById('box');
+console.log(box)
 
-function isBudgetEnough() {
-    let area = [];
-    shoppingMallData.shops.forEach((arr,i) => {
-        const a = arr.width * arr.length;
-        area[i] = a
-    });
-    let mainArea = 0;
-    for (i of area) {
-        mainArea += i
-    }
-    const VolumeofСenter = mainArea * shoppingMallData.height;
-    const valueofCenter = shoppingMallData.moneyPer1m3 * VolumeofСenter;
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-    if (valueofCenter <= shoppingMallData.budget) {
-        console.log(`Volume of shopping center - ${VolumeofСenter}\nValue of shopping center - ${valueofCenter}
-    `)
-    } else {
-        console.log(`Value of shopping center - ${valueofCenter}, but it is not enough`)
-    }
+const circles = document.getElementsByClassName('circle');
 
-}
+console.log(circles);
 
-isBudgetEnough()
+const heart = document.querySelectorAll(".heart");
+
+heart.forEach(item => {
+    console.log(item)
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+
