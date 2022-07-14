a5da2113e9c5c0fff75b7bcee52bb4508f65b3bf
Aikyn <210101044@stu.sdu.edu.kz>
2022-07-14 20:09:27 +0500

    updated v2

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-const box = document.getElementById('box'),
-    btns = document.getElementsByTagName('button'),
-    circles = document.getElementsByClassName('circle'),
-    hearts = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart'),
-    wrapper = document.querySelector('.wrapper');
+const box = document.getElementById('box');
+console.log(box)
 
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-btns[2].style.borderRadius = '100%';
+const circles = document.getElementsByClassName('circle');
 
-circles[0].style.backgroundColor = 'yellow';
+console.log(circles);
 
-box.style.cssText = 'background-color: blue; width: 500px';
+const heart = document.querySelectorAll(".heart");
 
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue'
-})
+heart.forEach(item => {
+    console.log(item)
+});
 
-const div = document.createElement('div');
-// const text = document.createTextNode('Hello world')
-
-div.classList.add('black');
-
-wrapper.appendChild(div);
-
-// wrapper.append(div)
-// wrapper.prepend(div);
-
-// hearts[0].before(div);
-// hearts[0].after(div);
-
-//
-// wrapper.insertBefore(div,hearts[0])
-// circles[0].remove();
-// hearts[0].replaceWith(circles[0]);
-// wrapper.removeChild(circles[0], hearts[0]);
-
-// div.innerHTML = '<h1>Helloworld</h1>';
-// div.textContent = "Hello";
-div.insertAdjacentHTML('afterbegin','<h2>Hello</h2>')
-
-
-
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
 
